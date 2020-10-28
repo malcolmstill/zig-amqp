ce7a893da5b117ee07e9e784815cbc559952b98b
Malcolm Still <malcolm.still@gmail.com>
2020-10-28 04:15:30 +0000

    Print cluster_name

--- src/init.zig
+++ src/init.zig
@@ -3,19 +3,21 @@ const proto = @import("protocol.zig");
 const Table = @import("table.zig").Table;
 
 fn connection_start (version_major: u8, version_minor: u8, server_properties: Table, mechanisms: []u8, locales: []u8) !void {
-    std.debug.warn("Connected to AMQP server (version {}.{})\nmechanisms: {}\nlocale: {}\n", .{
+    var sp = server_properties;
+    const host = sp.lookup([]u8, "cluster_name");
+    std.debug.warn("Connected to {} AMQP server (version {}.{})\nmechanisms: {}\nlocale: {}\n", .{
+        host,
         version_major,
         version_minor,
         mechanisms,
         locales
     });
 
-    var sp = server_properties;
-    std.debug.warn("product: {}\n", .{ sp.lookup([]u8, "product") });
-    std.debug.warn("platform: {}\n", .{ sp.lookup([]u8, "platform") });
-    if (sp.lookup(Table, "capabilities")) |*caps| {
-        std.debug.warn("\tconnection.blocked: {}\n", .{ caps.lookup(bool, "connection.blocked") });
-    }
+    // std.debug.warn("product: {}\n", .{ sp.lookup([]u8, "product") });
+    // std.debug.warn("platform: {}\n", .{ sp.lookup([]u8, "platform") });
+    // if (sp.lookup(Table, "capabilities")) |*caps| {
+        // std.debug.warn("\tconnection.blocked: {}\n", .{ caps.lookup(bool, "connection.blocked") });
+    // }
 }
 
 pub fn init() void {
