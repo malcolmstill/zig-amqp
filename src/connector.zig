56cc31243ea634283dcfa62aedbbc9dc717fcbdd
Malcolm Still <malcolm.still@gmail.com>
2020-12-18 08:49:27 +0000

    Factor out dispatchMethod

--- src/connector.zig
+++ src/connector.zig
@@ -34,56 +34,11 @@ pub const Connector = struct {
         const header = try self.rx_buffer.readFrameHeader();
 
         switch (header.@"type") {
+            // TODO: if we have a failure here, we probably still want to
+            //       copy further frames that exist to the front of the buffer
+            //       so they can be processed
             .Method => {
-                // 2a. The frame header says this is a method, attempt to read
-                // the method header
-                const method_header = try self.rx_buffer.readMethodHeader();
-                const class = method_header.class;
-                const method = method_header.method;
-
-                var sync_resp_ok = false;
-
-                // 3a. If this is a synchronous call, we expect expected_response to be
-                // non-null and to provide the expected class and method of the response
-                // that we're waiting on. That class and method is checked for being
-                // a synchronous response and then we compare the class / method from the
-                // header with expected_response and error if they don't match.
-                if (expected_response) |expected| {
-                    const is_synchronous = try proto.isSynchronous(class, method);
-
-                    if (is_synchronous) {
-                        // if (class != expected.class) return error.UnexpectedResponseClass;
-                        // if (method != expected.method) return error.UnexpectedResponseClass;
-                        if (class == expected.class and method == expected.method) {
-                            sync_resp_ok = true;
-                        } else {
-                            if (class == proto.CHANNEL_CLASS and method == proto.Channel.CLOSE_METHOD) {
-                                proto.dispatchCallback(self, class, method) catch |err| {
-                                    switch (err) {
-                                        error.ChannelError => {
-                                            self.connection.free_channel(self.channel);
-                                            return err;
-                                        },
-                                        else => return err,
-                                    }
-                                };
-                            }
-
-                            if (class == proto.CONNECTION_CLASS and method == proto.Connection.CLOSE_METHOD) {
-                                try proto.dispatchCallback(self, class, method);
-                            }
-
-                            return error.UnexpectedSync;
-                        }
-                    } else {
-                        sync_resp_ok = true;
-                    }
-                }
-
-                // 4a. Finally dispatch the class / method
-                // const connection: *proto.Connection = @fieldParentPtr(proto.Connection, "conn", self);
-                try proto.dispatchCallback(self, class, method);
-                return sync_resp_ok;
+                return self.dispatchMethod(expected_response);
             },
             .Heartbeat => {
                 if (std.builtin.mode == .Debug) std.debug.warn("Got heartbeat\n", .{});
@@ -93,6 +48,58 @@ pub const Connector = struct {
                 return false;
             },
         }
+    }
+
+    fn dispatchMethod(self: *Self, expected_response: ?ClassMethod) !bool {
+        // 2a. The frame header says this is a method, attempt to read
+        // the method header
+        const method_header = try self.rx_buffer.readMethodHeader();
+        const class = method_header.class;
+        const method = method_header.method;
+
+        var sync_resp_ok = false;
+
+        // 3a. If this is a synchronous call, we expect expected_response to be
+        // non-null and to provide the expected class and method of the response
+        // that we're waiting on. That class and method is checked for being
+        // a synchronous response and then we compare the class / method from the
+        // header with expected_response and error if they don't match.
+        if (expected_response) |expected| {
+            const is_synchronous = try proto.isSynchronous(class, method);
+
+            if (is_synchronous) {
+                // if (class != expected.class) return error.UnexpectedResponseClass;
+                // if (method != expected.method) return error.UnexpectedResponseClass;
+                if (class == expected.class and method == expected.method) {
+                    sync_resp_ok = true;
+                } else {
+                    if (class == proto.CHANNEL_CLASS and method == proto.Channel.CLOSE_METHOD) {
+                        proto.dispatchCallback(self, class, method) catch |err| {
+                            switch (err) {
+                                error.ChannelError => {
+                                    self.connection.free_channel(self.channel);
+                                    return err;
+                                },
+                                else => return err,
+                            }
+                        };
+                    }
+
+                    if (class == proto.CONNECTION_CLASS and method == proto.Connection.CLOSE_METHOD) {
+                        try proto.dispatchCallback(self, class, method);
+                    }
+
+                    return error.UnexpectedSync;
+                }
+            } else {
+                sync_resp_ok = true;
+            }
+        }
+
+        // 4a. Finally dispatch the class / method
+        // const connection: *proto.Connection = @fieldParentPtr(proto.Connection, "conn", self);
+        try proto.dispatchCallback(self, class, method);
+        return sync_resp_ok;
     }
 };
 
