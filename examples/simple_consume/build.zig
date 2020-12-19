05c83e87f1767117fc358f6d5d698c859d2c3fc6
Malcolm Still <malcolm.still@gmail.com>
2020-12-19 20:35:14 +0000

    Move simple_client to simple_consume

--- examples/simple_consume/build.zig
+++ examples/simple_consume/build.zig
@@ -11,7 +11,7 @@ pub fn build(b: *Builder) void {
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("simple_client", "src/main.zig");
+    const exe = b.addExecutable("simple_consume", "src/main.zig");
     exe.setTarget(target);
     exe.setBuildMode(mode);
     exe.addPackagePath("amqp", "../../src/amqp.zig");
