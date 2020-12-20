5c9070d049274b573e4730591abe8f769a6fef37
Malcolm Still <malcolm.still@gmail.com>
2020-12-20 03:55:05 +0000

    examples: simple_publish_single

---
+++ examples/simple_publish_single/src/main.zig
@@ -0,0 +1,19 @@
+const std = @import("std");
+const amqp = @import("amqp");
+
+var rx_memory: [4096]u8 = undefined;
+var tx_memory: [4096]u8 = undefined;
+
+pub fn main() !void {
+    var conn = amqp.init(rx_memory[0..], tx_memory[0..]);
+    const addr = try std.net.Address.parseIp4("127.0.0.1", 5672);
+    try conn.connect(addr);
+
+    var ch = try conn.channel();
+    _ = try ch.queueDeclare("simple_publish", amqp.Queue.Options{}, null);
+
+    var i: usize = 0;
+    while (i < 100_000) : (i += 1) {
+        try ch.basicPublish("", "simple_publish", "hello world", amqp.Basic.Publish.Options{});
+    }
+}
