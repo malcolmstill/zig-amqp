9531c592255017e3aca67ef693d2402d57ed838b
Malcolm Still <malcolm.still@gmail.com>
2020-12-21 13:44:51 +0000

    WIP: multiple channels

--- examples/simple_consume_two_channels/src/main.zig
+++ examples/simple_consume_two_channels/src/main.zig
@@ -1,16 +1,16 @@
 const std = @import("std");
 const amqp = @import("amqp");
 
-var rx_memory: [4096]u8 = undefined;
-var tx_memory: [4096]u8 = undefined;
+var memory: [3][2][4096]u8 = undefined;
 
 pub fn main() !void {
-    var conn = amqp.init(rx_memory[0..], tx_memory[0..]);
+    var conn = amqp.init(memory[0][0][0..], memory[0][1][0..]);
     const addr = try std.net.Address.parseIp4("127.0.0.1", 5672);
     try conn.connect(addr);
 
-    var ch1 = try conn.channel();
-    var ch2 = try conn.channel();
+    var ch1 = try conn.channel(memory[1][0][0..], memory[1][1][0..]);
+    ch1.atest(1);
+    var ch2 = try conn.channel(memory[2][0][0..], memory[2][1][0..]);
 
     var consumer1 = try ch1.basicConsume("simple_publish", .{ .no_ack = true }, null);
     var consumer2 = try ch2.basicConsume("simple_publish", .{ .no_ack = true }, null);
